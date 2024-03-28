3abf7b6006cff180957991c993a510ccd009b723
Ulysse AUPETIT <uaupetit@bess-f4r3s6.clusters.42paris.fr>
2024-03-28 17:19:23 +0100

    boucle a peu pres fonctionnelle

--- main.cpp
+++ main.cpp
@@ -8,28 +8,32 @@ int main(int ac, char **av)
         {
             Server server(av[1]);
             socklen_t client_addr_len = sizeof(struct sockaddr);
+            struct sockaddr client_addr;
+            int tmp = accept(server.getSocket(), (struct sockaddr *)&client_addr, &client_addr_len);
+            server.setClientSocket(tmp);
+            int i = 0;
             while (true)
             {
-                struct sockaddr client_addr;
                 /*
                 accepter demande de connexion client
                 client addr = pointeur vers structur ou accept rempli les info de la socket client
                 len = len de la struct
                 */
-                int tmp = accept(server.getSocket(), (struct sockaddr *)&client_addr, &client_addr_len);
-                server.setClientSocket(tmp);
                 if (server.getClientSocket() != -1)
                 {
-                    printf("New connection, Socket fd : %d, client fd : %d\n", server.getSocket(), server.getClientSocket());
+                    //printf("New connection, Socket fd : %d, client fd : %d\n", server.getSocket(), server.getClientSocket());
                     char buffer[1024];
                     ssize_t bytes_received = recv(server.getClientSocket(), buffer, sizeof(buffer), 0);
+                    i++;
                     buffer[bytes_received] = '\0'; 
-                    std::cout << buffer << std::endl;
+                        std::cout << buffer << std::endl;
                     //parser buffer et foutre dans client
                  //   server.parser(buffer);
                     const char *buf = ":localhost 001 uaupetit :Welcome to the Internet Relay Network uaupetit!uaupetit\r\n";
-                    ssize_t i = send(server.getClientSocket(), buf, strlen(buf), 0);
-                    (void)i;
+                    if (i == 1)
+                    {    ssize_t j = send(server.getClientSocket(), buf, strlen(buf), 0);
+                        (void)j;
+                    }//bytes_received = 0;
                 }
             }
         }
